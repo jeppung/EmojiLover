9366ebab5748817a6b05e256fa021de97e180a3e
Joshua Pulung <jeppung@gmail.com>
2023-07-01 18:52:15 +0700

    Initial Commit

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Joshua Pulung on 30/06/23.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 😍, 👨‍💻, 🚀, 😅
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .👨‍💻
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Joshua Pulung on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
